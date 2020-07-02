7a1290b7f8d39636e425ad16a5ec5951bb7dc2d7
unknown <18ucs081@lnmiit.ac.in>
2020-07-02 20:35:08 +0530

    Before Data Normalization

--- my-app/src/pages/collection/collection.component.jsx
+++ my-app/src/pages/collection/collection.component.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { selectShopCollection } from '../../redux/shop/shop.selector';
 
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-    console.log(collection);
+    const { title, items } = collection;
 
+    const renderCollectionItem = () => {
+        return items.map(item => <CollectionItem key={item.id} item={item} />);
+    };
     return (
         <div className="collection-page">
-            <h2>CollectionPage</h2>
+            <h2 className="title">{title}</h2>
+            <div className="items">{renderCollectionItem()}</div>
         </div>
     );
 };
