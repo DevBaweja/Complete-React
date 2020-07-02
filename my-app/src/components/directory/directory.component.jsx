7841af9210576df8797f4ccf2721a066005d2e0c
unknown <18ucs081@lnmiit.ac.in>
2020-07-02 13:42:36 +0530

    Directory state to redux

--- my-app/src/components/directory/directory.component.jsx
+++ my-app/src/components/directory/directory.component.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selector';
 
 import MenuItem from '../menu-item/menu-item.component';
-import sections from './sections.data';
+
 import './directory.styles.scss';
 
-class Directory extends React.Component {
-    state = {
-        sections,
+const Directory = ({ sections }) => {
+    const renderMenuItem = () => {
+        return sections.map(({ id, ...other }) => <MenuItem key={id} {...other} />);
     };
 
-    renderMenuItem = () => {
-        return this.state.sections.map(({ id, ...other }) => <MenuItem key={id} {...other} />);
-    };
+    return <div className="directory-menu">{renderMenuItem()}</div>;
+};
 
-    render() {
-        return <div className="directory-menu">{this.renderMenuItem()}</div>;
-    }
-}
-
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections,
+});
+export default connect(mapStateToProps)(Directory);
