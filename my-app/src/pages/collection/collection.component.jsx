0ca8c6d07f58a547f9f66b786d77fc8156eaad89
unknown <18ucs081@lnmiit.ac.in>
2020-07-19 12:48:39 +0530

    useContext Hook

--- my-app/src/pages/collection/collection.component.jsx
+++ my-app/src/pages/collection/collection.component.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { selectShopCollection } from '../../redux/shop/shop.selector';
+import React, { useContext } from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
@@ -9,27 +7,19 @@ import CollectionsContext from '../../contexts/collections/collections.context';
 import './collection.styles.scss';
 
 const CollectionPage = ({ match }) => {
-    const renderCollectionItem = items => {
+    const collections = useContext(CollectionsContext);
+    const collection = collections[match.params.collectionType];
+    const { title, items } = collection;
+
+    const renderCollectionItem = () => {
         return items.map(item => <CollectionItem key={item.id} item={item} />);
     };
     return (
-        <CollectionsContext.Consumer>
-            {collections => {
-                const collection = collections[match.params.collectionType];
-                const { title, items } = collection;
-
-                return (
-                    <div className="collection-page">
-                        <h2 className="title">{title}</h2>
-                        <div className="items">{renderCollectionItem(items)}</div>
-                    </div>
-                );
-            }}
-        </CollectionsContext.Consumer>
+        <div className="collection-page">
+            <h2 className="title">{title}</h2>
+            <div className="items">{renderCollectionItem()}</div>
+        </div>
     );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-    collection: selectShopCollection(ownProps.match.params.collectionType)(state),
-});
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
