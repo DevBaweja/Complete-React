4d6cac7b91ba42e210e9eb948b2aba22c019eabb
unknown <18ucs081@lnmiit.ac.in>
2020-08-28 15:19:56 +0530

    Web apps

--- 29. Progressive Web App/client/src/index.js
+++ 29. Progressive Web App/client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate as Persist } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App.jsx';
@@ -19,3 +20,5 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
+
+serviceWorker.register();
--- 29. Progressive Web App/server.js
+++ 29. Progressive Web App/server.js
@@ -24,6 +24,10 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 // Routes
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
+
 app.post('/payment', (req, res) => {
     const body = {
         source: req.body.token.id,
