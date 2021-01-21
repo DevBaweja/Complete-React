b90eafcbaf92d1637a96435b0ad8ba0e4c7b9dd6
Dev Baweja <18ucs081@lnmiit.ac.in>
2021-01-22 01:27:50 +0530

    Testing

---
+++ 31. Testing/client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate as Persist } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+import App from './App.jsx';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <Persist persistor={persistor}>
+                <App />
+            </Persist>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
+
+serviceWorker.register();
